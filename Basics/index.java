5b7ef7148acfd1d26d654a1deb5fd67f224dfc16
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-01-01 16:04:44 +0530

    Basics of java

--- Basics/index.java
+++ Basics/index.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 class index{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name= input.nextLine();
+        System.out.println("Hello "+ name);
+
+        //Sum of two numbers
+        System.out.println("Enter the first digit");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second digit");
+        int num2 = input.nextInt();
+        int sum = num1+num2;
+        System.out.println("The sum of the given two numbers is: "+ sum);
+
     }
 }
