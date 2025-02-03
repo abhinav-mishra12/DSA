76aee6e789dcdbc79cfcf47286a86eb326418a54
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-02-03 20:28:09 +0530

    Basics

---
+++ Basics/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.next();
+        System.out.println("Goodmorning "+name);
+        input.close();
+    }
+}
