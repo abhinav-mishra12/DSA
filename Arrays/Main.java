110b00aae65435b0f86049f43d84f8801c14f599
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-02-05 12:01:00 +0530

    Basic of Arrays

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,9 +1,9 @@
-package Arrays;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         int[] arr = new int[5];
         Scanner input = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
@@ -12,6 +12,30 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]+" ");
         }
+        System.out.println(Arrays.toString(arr));
+        
+
+        String[] str = new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i]=input.next();
+        }
+        System.out.println(Arrays.toString(str));
+
+        //Modify
+        str[2]="Abhinav";
+        System.out.println(Arrays.toString(str));
         input.close();
+
+        //Passing Functions
+        int[] Arr = {0,1,2,3,4,5};
+        System.out.println(Arrays.toString(Arr));
+        change(Arr);
+
+        
+        
+
+    }
+    static void change(int[] arr){
+        arr[0]=99;
     }
 }
