bca683409e26a039879502a7cfcb15973db867b9
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-02-23 21:33:31 +0530

    Questions

---
+++ Arrays/Questions/runningSum.java
@@ -0,0 +1,61 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+public class runningSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            nums.add(input.nextInt());
+        }
+
+        //Convert ArrayList into int[] array 
+        int[] numsArray = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            numsArray[i]=nums.get(i);
+        }
+
+        //Call the method
+        int[] ans = runningSum(numsArray);
+        System.out.print(Arrays.toString(ans));
+        
+        input.close();
+    }
+
+    public static int[] runningSum(int[] nums){
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < ans.length; i++) {
+         if (i==0) {
+            ans[i]=nums[i];
+         }   
+        else if (i>0) {
+            for (int j = 0; j <= i; j++) {
+                ans[i]= nums[i-1]+nums[i];
+            }
+        }
+        }
+        return ans;
+    }
+}
