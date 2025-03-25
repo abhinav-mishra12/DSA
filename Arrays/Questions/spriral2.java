a28539d7c79910d3278704b3de5706a49375bec2
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-03-25 12:09:01 +0530

    spriral2

--- Arrays/Questions/spriral2.java
+++ Arrays/Questions/spriral2.java
@@ -1,38 +1,77 @@
+// Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+ 
+
+// Example 1:
+
+
+// Input: n = 3
+// Output: [[1,2,3],[8,9,4],[7,6,5]]
+// Example 2:
+
+// Input: n = 1
+// Output: [[1]]
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class spriral2 {
     public static void main(String[] args) {
-        class Solution {
-            public int[][] generateMatrix(int n) {
-                int[][] mat = new int[n][n];
-                int left=0,top=0,right=n-1,bottom=n-1;
-                int element=1;
-                while(left<=right && top<=bottom){
-                    for(int i=left;i<=right;i++){
-                        mat[top][i] = element;
-                        element++; 
-                    }
-                    top++;
-        
-                    for(int i=top;i<=bottom;i++){
-                        mat[i][right]=element;
-                        element++;
-                    }
-                    right--;
-                    
-                    for(int i=right;i>=left;i--){
-                        mat[bottom][i]=element;
-                        element++;
-                    }
-                    bottom--;
-        
-                    for(int i=bottom;i>=top;i--){
-                        mat[i][left]=element;
-                        element++;
-                    }
-                    left++;
-                }
-                return mat;
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter the value of n: ");
+       int n = input.nextInt();
+       int[][] natrix = generateMatrix(n);
+       for (int i = 0; i < n; i++) {
+        System.out.print(Arrays.toString(natrix[i]));    
+       }    
+       input.close();;
+
+    }
+
+    public static int[][] generateMatrix(int n){
+        int[][] matrix = new int[n][n];
+        int rowBegin=0;
+        int colBegin=0;
+        int colEnd=n-1;
+        int rowEnd=n-1;
+        int element = 1;
+
+        while (rowBegin<=rowEnd && colBegin<=colEnd){
+            
+            //Traverse Right
+            for (int i = colBegin; i <= colEnd; i++) {
+                matrix[rowBegin][i]=element;
+                element++;
             }
+            rowBegin++;
+
+             //Traverse Down
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                matrix[i][colEnd]=element;
+                element++;
+            }
+            colEnd--;
+
+            //Traverse Left
+            if (rowBegin<=rowEnd) {
+                for (int i = colEnd; i >= colBegin; i--) {
+                    matrix[rowEnd][i]=element;
+                    element++;
+                }
+            }
+            rowEnd--;
+
+            //Traverse up
+            if (colBegin<=colEnd) {
+                for (int i = rowEnd; i >= rowBegin; i--){
+                    matrix[i][colBegin]=element;
+                    element++;
+                }
+            }
+            colBegin++;
+
         }
-        
+
+        return matrix;
     }
 }
