63103a97fa7395db6e8990acc42c6012863c0ce6
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-04-02 20:15:10 +0530

    Main

--- Bitwise Operator/Main.java
+++ Bitwise Operator/Main.java
@@ -1,5 +1,17 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hi");
+        int[] arr = {2, 3, 3, 4, 2, 6, 4};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int[] arr) {
+        int unique = 0;
+
+        for(int n : arr) {
+            unique ^= n;
+        }
+
+        return unique;
     }
 }
