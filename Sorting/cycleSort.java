15f2f67b03687908550cd5d15bfb36eb1dc3c61c
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-04-21 09:23:46 +0530

    cycleSort

---
+++ Sorting/cycleSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cycleSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=input.nextInt();
+        }
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+        input.close();
+    }
+    public static void sort(int[] arr){
+        int i=0;
+        while (i!=arr.length) {
+            int correct = arr[i]-1;
+            if (arr[i]!=arr[correct]) {
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
