09e14557bca83895cea2d9a24b40956bcc79e3d4
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-05-03 10:31:59 +0530

    searchA2DMatrix

--- Searching/Binary Search/Questions/searchA2DMatrix.java
+++ Searching/Binary Search/Questions/searchA2DMatrix.java
@@ -22,39 +22,78 @@ public class searchA2DMatrix {
     public static void main(String[] args) {
         class Solution {
             public boolean searchMatrix(int[][] matrix, int target) {
-                int top = 0;
-                int bot = matrix.length - 1;
+                int row = matrix.length;
+                int col = matrix[0].length;  // be cautious, matrix may be empty
         
-                while (top <= bot) {
-                    int mid = (top + bot) / 2;
-        
-                    if (matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] > target) {
-                        break;
-                    } else if (matrix[mid][0] > target) {
-                        bot = mid - 1;
-                    } else {
-                        top = mid + 1;
-                    }
+                if(row==1){
+                    return binarySearch(matrix, 0, 0, col-1, target);
                 }
         
-                int row = (top + bot) / 2;
+                int rStart = 0;
+                int rEnd = row-1;
+                int cMid = col/2;
         
-                int left = 0;
-                int right = matrix[row].length - 1;
-        
-                while (left <= right) {
-                    int mid = (left + right) / 2;
-        
-                    if (matrix[row][mid] == target) {
+                // Run the loop till two rows are remaining
+                while(rStart<(rEnd-1)){  // While this is true it will have more than two rows
+                    int mid = rStart+(rEnd-rStart)/2;
+                    if(matrix[mid][cMid]==target){
                         return true;
-                    } else if (matrix[row][mid] > target) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
+                    }
+                    else if(matrix[mid][cMid]<target){
+                    rStart=mid;
+                    }
+                    else if(matrix[mid][cMid]>target){
+                    rEnd=mid;
                     }
                 }
         
-                return false;        
+                //Now we have 2 rows 
+                //Check whether the target is in the cols of 2 rows
+                if(matrix[rStart][cMid]==target){
+                    return true;
+                }
+        
+                if(matrix[rStart+1][cMid]==target){
+                    return true;
+                }
+        
+                // Search in 1st half
+                if(cMid - 1 >= 0 && target <= matrix[rStart][cMid - 1]){
+                    return binarySearch(matrix, rStart, 0, cMid - 1, target);
+                }
+                // Search in 2nd half
+                if(cMid + 1 < col && target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][col - 1]){
+                    return binarySearch(matrix, rStart, cMid + 1, col - 1, target);
+                }
+                // Search in 3rd half
+                if(cMid - 1 >= 0 && target <= matrix[rStart + 1][cMid - 1]){
+                    return binarySearch(matrix, rStart + 1, 0, cMid - 1, target);
+                }
+                // Search in 4th half
+                if(cMid + 1 < col && target >= matrix[rStart + 1][cMid + 1]){
+                    return binarySearch(matrix, rStart + 1, cMid + 1, col - 1, target);
+                }
+        
+        
+                return false;
+        
+            }
+        
+            // Search in the row provided in between the colums
+            static boolean binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target){
+                while(cStart<=cEnd){
+                    int mid = cStart+(cEnd-cStart)/2;
+                    if(matrix[row][mid]==target){
+                        return true;
+                    }
+                    if(matrix[row][mid]<target){
+                        cStart=mid+1;
+                    }
+                    else{
+                        cEnd=mid-1;
+                    }
+                }
+                return false;
             }
         }
     }
