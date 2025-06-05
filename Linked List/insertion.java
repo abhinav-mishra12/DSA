5901aacdae219c80f38d2eef1a2406388b88244d
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-06-05 17:39:10 +0530

    insertion

--- Linked List/insertion.java
+++ Linked List/insertion.java
@@ -1,3 +1,17 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
 public class insertion {
     public static void main(String[] args) {
         int[] arr = {3,5,8,7,6,5};
