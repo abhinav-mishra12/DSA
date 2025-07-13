22ffc7a20028d7ac158ba629473023cd94f5c73e
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-07-13 10:51:14 +0530

    postfixToInfix

---
+++ Stack and Queue/postfixToInfix.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class postfixToInfix {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack();
+        String s = "ab-de+f*/";
+        int i = 0;
+        while(i<s.length()){
+            char ch = s.charAt(i);
+            if ((ch >= 'A' && ch <= 'Z') ||
+                (ch >= 'a' && ch <= 'z') ||
+                (ch >= '0' && ch <= '9')){
+                    st.push(String.valueOf(ch));
+                }
+            else{
+                String t1 = st.pop();
+                String t2 = st.pop();
+                String con = '(' + t2 + ch + t1 + ')';
+                st.push(con); 
+            }
+            i++;
+        }
+
+        System.out.println(st.peek());
+    }
+}
