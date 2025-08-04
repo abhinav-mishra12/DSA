12e5175617e8ab715f699197f809c54da6bebe32
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-08-04 10:11:42 +0530

    binaryTreeRightSideView

---
+++ Binary Tree/binaryTreeRightSideView.java
@@ -0,0 +1,89 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,2,3,null,5,null,4]
+
+// Output: [1,3,4]
+
+// Explanation:
+
+
+
+// Example 2:
+
+// Input: root = [1,2,3,4,null,null,null,5]
+
+// Output: [1,3,4,5]
+
+// Explanation:
+
+
+
+// Example 3:
+
+// Input: root = [1,null,3]
+
+// Output: [1,3]
+
+// Example 4:
+
+// Input: root = []
+
+// Output: []
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class binaryTreeRightSideView {
+    public static void main(String[] args) {
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // number of nodes in this level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+
+                // if it's the last node in this level, add to result
+                if (i == size - 1) {
+                    result.add(current.val);
+                }
+
+                // enqueue left and right children
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+            }
+        }
+
+        return result;
+    }
+}
+
+    }
+}
