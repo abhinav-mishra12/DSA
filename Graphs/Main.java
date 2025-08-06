1cf2a945df5f414229d74dfe153bb6c5007d38d5
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-08-06 15:18:09 +0530

    graph

---
+++ Graphs/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+
+        // Creating an adjacency list of size n + 1
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // Initialize each list
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        // edge 1--2
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+
+        // edge 2--3
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        // edge 1--3
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+
+        // Printing the adjacency list
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " -> ");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
