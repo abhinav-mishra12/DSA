1f2268a619b054f2440325f72c9f96341301430f
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-08-06 10:50:07 +0530

    deleteNodeInABST

---
+++ Binary Search Tree/deleteNodeInABST.java
@@ -0,0 +1,87 @@
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+// Basically, the deletion can be divided into two stages:
+
+// Search for a node to remove.
+// If the node is found, delete the node.
+ 
+
+// Example 1:
+
+
+// Input: root = [5,3,6,2,4,null,7], key = 3
+// Output: [5,4,6,2,null,null,7]
+// Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+// One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+// Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,7], key = 0
+// Output: [5,3,6,2,4,null,7]
+// Explanation: The tree does not contain a node with value = 0.
+// Example 3:
+
+// Input: root = [], key = 0
+// Output: []
+public class deleteNodeInABST {
+    public static void main(String[] args) {
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+
+        if(key<root.val){
+            root.left=deleteNode(root.left, key);
+        }
+        else if(key>root.val){
+            root.right=deleteNode(root.right,key);
+        }
+        else{
+            //Found the node to delete
+            if(root.left==null){
+                return root.right;//only right child
+            }
+
+            else if(root.right==null){
+                return root.left; // only left child
+            }
+
+            else{
+                // Node has two children
+                TreeNode successor = findMin(root.right);
+                root.val = successor.val; // Replace value
+                root.right = deleteNode(root.right, successor.val); // Delete successor
+            }
+        }
+
+        return root;
+
+    }
+
+    // Helper to find the minimum value node in a subtree
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
+    }
+}
