0de8f6c2ef36eb77c5bab655a90c152c09dd4060
abhinav-mishra12 <mishra.k.abhinav@gmail.com>
2025-10-08 08:56:50 +0530

    Main

---
+++ DP/Main.java
@@ -0,0 +1,52 @@
+public class Main{
+    public static void main(String[] args){
+    // public class FibonacciMemo {
+    // static int[] memo = new int[100]; // adjust size as needed
+
+    // public static int fib(int n) {
+    //     // Base cases
+    //     if (n <= 1)
+    //         return n;
+
+    //     // If value already computed, return it
+    //     if (memo[n] != 0)
+    //         return memo[n];
+
+    //     // Otherwise, compute and store in memo
+    //     memo[n] = fib(n - 1) + fib(n - 2);
+    //     return memo[n];
+    // }
+
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     System.out.println("Fibonacci number using Memoization: " + fib(n));
+    //     }  
+    // }
+
+    // public class FibonacciTab {
+    // public static int fib(int n) {
+    //     // Base cases
+    //     if (n <= 1)
+    //         return n;
+
+    //     int[] dp = new int[n + 1];
+    //     dp[0] = 0;
+    //     dp[1] = 1;
+
+    //     for (int i = 2; i <= n; i++) {
+    //         dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
+
+    //     return dp[n];
+    // }
+
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     System.out.println("Fibonacci number using Tabulation: " + fib(n));
+    //     }
+    // }
+
+
+
+    }
+}
